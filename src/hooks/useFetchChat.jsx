3c6819d1dba34521b56d808f35c1070725dd47bf
Rudra Pratap Singh <rudrapsingh121@gmail.com>
2025-10-25 11:54:52 +0530

    hook for fetching chats

---
+++ src/hooks/useFetchChat.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const useFetchChat = (toUserId) => {
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const fetchChat = async () => {
+    try {
+      setLoading(true)
+      const res = await axios.get(
+        `http://localhost:1001/chat/getchats/${toUserId}`,
+        {
+          withCredentials: true,
+        }
+      );
+      setMessages(res?.data?.data);
+    } catch (error) {
+      setLoading(false)
+      console.log(error);
+    } finally { 
+      setLoading(false)
+    }
+  };
+
+  useEffect(() => {
+    fetchChat();
+  }, [toUserId]);
+  return { messages, setMessages , loading};
+};
+
+export default useFetchChat;
